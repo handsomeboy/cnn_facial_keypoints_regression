673420de4b0d550efe5159205dfb2242e49dab74
Krishnakumar Chandran <krishna-chandran@users.noreply.github.com>
2018-10-18 17:06:56 -0400

    Project completed

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -10,9 +9,9 @@ import torch.nn.init as I
 
 class Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self,weight):
         super(Net, self).__init__()
-        
+        k_height, k_width = weight.shape[2:]
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
@@ -20,9 +19,17 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=(k_height, k_width),bias=False)
+        self.conv1.weight = torch.nn.Parameter(weight)
+        self.conv2 = nn.Conv2d(4, 512, kernel_size=(k_height, k_width) )
+        self.conv3 = nn.Conv2d(512, 1026 ,kernel_size=(k_height, k_width))
+        self.pool = nn.MaxPool2d(4,4)
         
-        ## Note that among the layers to add, consider including:
+        self.fc1 = nn.Linear(4104,512)
+        self.fc1_drop = nn.Dropout(p=0.4)
+        self.fcfinal = nn.Linear(512, 136)
+        
+        # Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
 
@@ -30,8 +37,56 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = (x.view(x.size(0),-1))
+        #print(x.size())
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fcfinal(x)
         
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+
+# class Net(nn.Module):
+
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         ## TODO: Define all the layers of this CNN, the only requirements are:
+#         ## 1. This network takes in a square (same width and height), grayscale image as input
+#         ## 2. It ends with a linear layer that represents the keypoints
+#         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+#         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+#         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+#         self.conv1 = nn.Conv2d(1, 32, 3)
+#         self.conv2 = nn.Conv2d(32, 64, 3 )
+#         self.conv3 = nn.Conv2d(64, 512 ,3)
+#         self.pool = nn.MaxPool2d(3,3)
+        
+#         self.fc1 = nn.Linear(25088,512)
+#         self.fc1_drop = nn.Dropout(p=0.3)
+#         self.fcfinal = nn.Linear(512, 136)
+        
+#         ## Note that among the layers to add, consider including:
+#         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+
+        
+#     def forward(self, x):
+#         ## TODO: Define the feedforward behavior of this model
+#         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = self.pool(F.relu(self.conv3(x)))
+#         x = (x.view(x.size(0),-1))
+# #         print(x.size())
+#         x = F.relu(self.fc1(x))
+#         x = self.fc1_drop(x)
+#         x = self.fcfinal(x)
+        
+        
+#         # a modified x, having gone through all the layers of your model, should be returned
+#         return x
